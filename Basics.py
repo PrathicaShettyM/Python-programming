012e31d741f5a89c5ebffeacae74c4b60557ceec
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2023-11-09 18:30:36 +0530

    python basics

---
+++ Basics.py
@@ -0,0 +1,14 @@
+# taking input from the user
+# name = input("Enter your name : ")
+# print(name)
+
+# Note : the input function will take any input as string, so to avoid this do type casting
+# Enter_num = int(input("Enter an integer : "))
+# print("The integer is : ", Enter_num)
+# print(type(Enter_num))
+
+# Sum of 2 numbers
+num1 = int(input("Enter the value of num1 : "))
+num2 = int(input("Enter the value of num2 : "))
+sum = num1 + num2
+print("The sum of num1 and num2 is : ",sum)
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Python-deep-dive
 This repository consists of Python tutorial using Juypter Notebook
+1. Python Basics - input, output, varaibles, operators
