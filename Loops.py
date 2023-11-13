70176aec692ced8505e6b5faa9ee4e2774b85f23
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2023-11-13 17:45:52 +0530

    for and while loops

---
+++ Loops.py
@@ -0,0 +1,36 @@
+# for loop
+# range (0, n-1) : 0 to n-1 and i = i+1
+
+# for i in range(0,10):
+#     print("Hello python",i)
+
+# to increase the steps ex: i = i + 2
+# range : `range(start, end-1, step)`
+# if range(11) : then it will run from 0 to 11-1(10)
+
+# for i in range(0,10,2):
+#     print(i)
+
+# for _ in range(0,10,2):
+#     print("Hello world")
+
+# print elements of a list using for loop
+# list1 = [10,20,30,40,50]
+# fruits = ["apples", "bananas", "cherry", "grapes", "mango"]
+
+# for i in list1:
+#     print(i) 
+    
+# for i in fruits:
+#     print(i)
+
+# While loop : runs till a condition is true
+# i = 0
+# while i<10:
+#         print("Hello programming world", i)
+#         i+=1
+        
+# PATTERN PRINTING
+
+
+
